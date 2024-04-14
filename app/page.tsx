c71493e0fdf356c681afaeadadc9452c114d9bd8
Bert <le91081@gmail.com>
2024-04-14 16:38:45 +0800

    update: setup skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
@@ -17,6 +18,10 @@ export default function Home() {
 
       <section id="experience" className="snap-center">
         <WorkExperience />
+      </section>
+
+      <section id="skills" className="snap-start">
+        <Skills />
       </section>
     </main>
   );
