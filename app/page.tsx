ab4863515064d67aa0b531aefa3a50ee2130b966
Bert <le91081@gmail.com>
2024-04-14 17:02:26 +0800

    update: setup projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 
@@ -23,6 +24,10 @@ export default function Home() {
       <section id="skills" className="snap-start">
         <Skills />
       </section>
+
+      <div id="projects" className="snap-start">
+        <Projects />
+      </div>
     </main>
   );
 }
