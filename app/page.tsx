cb3585a66da422526013af0492add8988f5aa42a
Bert <le91081@gmail.com>
2024-04-15 20:29:29 +0800

    update: tailwind scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />
       <section id="hero" className="snap-start">
         <Hero />
